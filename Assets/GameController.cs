ed42290f0381eca629b7d9b679cf84898fb8ffdd
Your Name <paperez97@gmail.com>
2020-11-14 15:27:19 +0000

    Ponedor de torrecitas funcionando

---
+++ Assets/GameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public string selectedTower;
+
+    public void ChangeSelectedTower(string towerName)
+    {
+        selectedTower = towerName;
+    }
+}
---
+++ Assets/Scripts/TowerMaker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerMaker : MonoBehaviour
+{
+    public GameController gameController;
+    public GameObject notower;
+    public GameObject eiffel;
+    public GameObject khalifa;
+    GameObject towerToCreate;
+    GameObject createdTower;
+
+    void Start()
+    {
+        towerToCreate = notower;
+        createdTower = Instantiate(towerToCreate, gameObject.transform);
+    }
+    void OnMouseDown()
+    {
+        if (gameController.selectedTower == "Eiffel")
+        {
+            towerToCreate = eiffel;
+        }
+        else if (gameController.selectedTower == "Khalifa")
+        {
+            towerToCreate = khalifa;
+        }
+        if (gameController.selectedTower == "Erase")
+        {
+            towerToCreate = notower;
+        }
+
+        Destroy(createdTower);
+        createdTower = Instantiate(towerToCreate, gameObject.transform);
+    }
+}
