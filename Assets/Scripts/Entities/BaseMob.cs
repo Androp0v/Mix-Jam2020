450d7f2d213413162b79d6205c56321c5a8f5075
Raúl Montón Pinillos <raul_m_p@me.com>
2020-11-14 01:07:52 +0100

    Basic work on entities

---
+++ Assets/Scripts/Entities/BaseMob.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BaseMob : MonoBehaviour
+{   
+    // CLASS CONSTANTS
+    public const double simulationTimestep = 0.1;
+
+    // MOB STATS
+    [SerializeField]
+    public const float maxAge = 0; //Age until mob dies
+    [SerializeField]
+    public const int speed = 40; //Speed at which the mob moves
+    [SerializeField]
+    public const double seekingFoodRadius = 30; //Radius for seeking food
+
+    // MOB PROPERTIES
+    private Rigidbody2D rigidBody;
+    private BaseBehaviour behaviour;
+
+    // MOB STATUS
+    private int _hunger = 20;
+    private int _matingUrge = 10;
+    private double _age = 0;
+
+    public Vector2 oldDirection = new Vector2(0,0);
+    public Vector2 nextDirection = new Vector2(0,0);
+
+    // Start is called before the first frame update (setup mob here)
+    void Start()
+    {
+        // Set the age to zero
+        _age = 0.0;
+        // Initialize behaviour
+        behaviour = new BaseBehaviour();
+        behaviour.attachedMob = this;
+        // Initialize moving direction
+        oldDirection = new Vector2(Random.Range(-1f, 1f), (Random.Range(-1f, 1f))).normalized;
+        nextDirection = new Vector2(Random.Range(-1f, 1f), (Random.Range(-1f, 1f))).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update age of the mob on every update
+        _age += simulationTimestep;
+        // Check if the mob is too old
+        if (_age > maxAge) {
+            OnDeath();
+        }
+    }
+
+    // OnDeath is called when the mob dies
+    public void OnDeath(){
+        Destroy(transform.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Entities/Behaviours/BaseBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BaseBehaviour
+{   
+    // BEHAVIOUR PROPERTIES
+    public BaseMob attachedMob;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Random Walk function (idle)
+
+    protected void randomWalk(Rigidbody2D _rigidBody)
+    {
+        // Random walking
+
+        if (attachedMob.oldDirection == new Vector2(0, 0) || attachedMob.nextDirection == new Vector2(0, 0))
+        {
+            attachedMob.oldDirection = new Vector2(Random.Range(-1f, 1f), (Random.Range(-1f, 1f))).normalized;
+            attachedMob.nextDirection = new Vector2(Random.Range(-1f, 1f), (Random.Range(-1f, 1f))).normalized;
+        }
+
+        /*if (_currentSteps == interpolatingSteps)
+        {
+            _oldDirection = _nextDirection;
+            _rigidBody.velocity = _nextDirection * (idlingSpeed * Time.deltaTime);
+
+            _nextDirection = _oldDirection.Rotate(Random.Range(-maxAngle, maxAngle));
+
+            _currentSteps = 0;
+        }
+        else
+        {
+            _movementDirection = Vector2.Lerp(_oldDirection, _nextDirection, (float)_currentSteps / interpolatingSteps).normalized;
+            _rigidBody.velocity = _movementDirection * (idlingSpeed * Time.deltaTime);
+
+            _currentSteps += 1;
+        }*/
+    }
+
+    public virtual void Move(Rigidbody2D rigidBody){
+        randomWalk(rigidBody);
+    }
+}
