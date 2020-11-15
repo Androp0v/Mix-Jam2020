186892b7620a188990b846d879cc871a47f10f63
Your Name <paperez97@gmail.com>
2020-11-15 01:33:03 +0000

    Plantita crece

--- Assets/GrowingFood.cs
+++ Assets/GrowingFood.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class GrowingFood : MonoBehaviour
 {
-    // Start is called before the first frame update
+    SpriteRenderer spriteRenderer;
+
+    public Sprite step1;
+    public Sprite step2;
+    public Sprite step3;
+    public Sprite step4;
+    Sprite[] spriteList;
+
+    int step = 0;
+    float stepTime = 1f;
+    float startTime;
+
+    public GameObject foodMob;
+
+
+
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startTime = Time.time;
+        spriteList = new Sprite[] { step1, step2, step3, step4 };
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - startTime > stepTime)
+        {
+            startTime = Time.time;
+            step++;
+        }
+        if (step < 4)
+        { spriteRenderer.sprite = spriteList[step]; }
+        else
+        {
+            Instantiate(foodMob).transform.position = transform.position;
+            Destroy(gameObject);
+        }
     }
+
+
 }
